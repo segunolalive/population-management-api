8cbda153a22e195134ba305c69f51632b3995b9e
Segun <segunolalive@gmail.com>
2019-01-09 16:52:40 +0100

    ignore coverage files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
   coverageReporters: ['lcov', 'html'],
   moduleNameMapper: {
     '^mongoose$': '<rootDir>/node_modules/mongoose'
